7a3c73b862e65b6b5c89c31945ae4640e08a8f20
Nathan Monroe <nmonroe.dev@gmail.com>
2024-12-15 20:10:44 -0600

    first save

---
+++ 1.js
@@ -0,0 +1,5 @@
+//Given an array of integers, return a new array with each value doubled.
+
+function double(array) {
+    return array.map(element => element * 2);
+};
---
+++ 3.js
@@ -0,0 +1,35 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num){
+    console.log(num);
+    return num.toString();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+const numberToString = num => `${num}`;
+
+
+
+
+
+
+
+
+
+
+
+
+numberToString(5);
+
+
